950e19c2f7db1f004f4461f9ad8b3c20af540060
Maryan Ivanyura <pfloydcomplex6@gmail.com>
2019-09-24 19:52:56 +0300

    Traffic implemented

--- src/index.js
+++ src/index.js
@@ -16,19 +16,91 @@ const Key = {
 const Setting = {
     start: false,
     score: 0,
-    speed: 3
+    speed: 3,
+    traffic: 3
+};
+
+const getLinesAmount = lineHeight => document.documentElement.clientHeight / lineHeight + 1;
+
+const moveRoad = () => {
+    const roadLines = document.querySelectorAll('.road-line');
+    roadLines.forEach(line => {
+        line.y += Setting.speed;
+        line.style.top = `${line.y}px`;
+
+        if (line.y >= document.documentElement.clientHeight) {
+            line.y = -100;
+        }
+    });
+};
+
+const moveOpponent = () => {
+    const opponents = document.querySelectorAll('.opponent');
+    opponents.forEach(opponent => {
+        opponent.y += Setting.speed / 2; // move multiplier
+        opponent.style.top = opponent.y + 'px';
+        if (opponent.y >= document.documentElement.clientHeight) {
+            opponent.y = -100 * Setting.traffic;
+            opponent.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50) /* car width */) + 'px';
+            // -100 === car height. must be
+        }
+    });
 };
 
 const playGame = () => {
     if (Setting.start) {
+        moveRoad();
+        moveOpponent();
+        if (Key.ArrowLeft && Setting.x > -40) {
+            Setting.x -= Setting.speed;
+        }
+
+        if (Key.ArrowRight && Setting.x < gameArea.offsetWidth - car.offsetWidth + 40) {
+            Setting.x += Setting.speed;
+        }
+
+        if (Key.ArrowUp && Setting.y > 50) {
+            Setting.y -= Setting.speed;
+        }
+
+        if (Key.ArrowDown && Setting.y < gameArea.offsetHeight - car.offsetHeight - 50) {
+            Setting.y += Setting.speed;
+        }
+
+        car.style.left = `${Setting.x}px`;
+        car.style.top = `${Setting.y}px`
+
         requestAnimationFrame(playGame);
     }
 };
 
 const startGame = () => {
     start.classList.add('hidden');
+    gameArea.classList.remove('hidden');
+
+    for (let i = 0; i < getLinesAmount(100); i++) {
+        const roadLine = document.createElement('div');
+        roadLine.classList.add('road-line');
+        roadLine.style.top = i * 100 + 'px';//`${i * 100}px`;
+        roadLine.y = i * 100;
+        gameArea.appendChild(roadLine);
+    }
+
+    for (let i = 0; i < getLinesAmount(100 * Setting.traffic); i++)  {
+        const opponent = document.createElement('div');
+        opponent.classList.add('opponent');
+        // 100 - car height ??
+        opponent.y = -100 * Setting.traffic * (i + 1);
+        opponent.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50) /* car width */) + 'px';
+        opponent.style.top = opponent.y + 'px';
+        //opponent.style.background = url
+        gameArea.appendChild(opponent);
+    }
+
     Setting.start = true;
     gameArea.appendChild(car);
+    Setting.x = car.offsetLeft;
+    Setting.y = car.offsetTop;
     requestAnimationFrame(playGame);
 };
 
